44f54183f85edc7023efd9cd3ab1f380e3d45ed0
qiujiahong <qiujh@szfangle.com>
2018-05-25 08:16:41 +0800

    添加数据库模型；

--- README.md
+++ README.md
@@ -16,4 +16,24 @@
 |2|jpa-many-to-many||
 
 
+## one to one
 
+![](./assets/one-to-one.png)
+
+```sql
+CREATE TABLE `author` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`name` varchar(20) NULL,
+`phone` varchar(20) NULL,
+`email` varchar(20) NULL,
+PRIMARY KEY (`id`)
+);
+CREATE TABLE `book` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`name` varchar(20) NULL,
+`isbn` varchar(20) NULL,
+`author_id` bigint(20) NULL,
+PRIMARY KEY (`id`)
+);
+ALTER TABLE `book` ADD CONSTRAINT `fk_author` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`);
+```
--- jpa-one-to-one/src/main/resources/sql/init.sql
+++ jpa-one-to-one/src/main/resources/sql/init.sql
@@ -1,21 +1,3 @@
--- DROP table  IF EXISTS book;
--- CREATE TABLE `book` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT,
---   `isbn` varchar(30) DEFAULT NULL,
---   `name` varchar(30) DEFAULT NULL,
---   `author_id` int,
---   PRIMARY KEY (`id`)
--- )DEFAULT CHARSET=utf8;
---
--- DROP table  IF EXISTS author;
--- CREATE TABLE `author` (
---   `id` bigint(20) NOT NULL  AUTO_INCREMENT,
---   `name` varchar(30) DEFAULT NULL,
---   `phone` varchar(30) DEFAULT NULL,
---   `email` varchar(30) DEFAULT NULL,
---   PRIMARY KEY (`id`)
--- )DEFAULT CHARSET=utf8;
-
 CREATE TABLE `author` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
 `name` varchar(20) NULL,
@@ -30,8 +12,6 @@ CREATE TABLE `book` (
 `author_id` bigint(20) NULL,
 PRIMARY KEY (`id`)
 );
-
-
 ALTER TABLE `book` ADD CONSTRAINT `fk_author` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`);
 
 insert into author (name,phone,email) values ('NICK','18022222222','nick@112.com');
