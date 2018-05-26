2f81e44770cc4ccc4adccd582ff2e4ba3d7e4e98
qiujh <qiujh@szfangle.com>
2018-05-27 07:38:58 +0800

    添加测试函数；

---
+++ jpa-many-to-many/src/test/java/com/nick/demo/JpaManyTomanyApplicationTests.java
@@ -0,0 +1,23 @@
+package com.nick.demo;/*
+ * @Author      : Nick
+ * @Description :
+ * @Date        : Create in 07:32 2018/5/27
+ **/
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class JpaManyTomanyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        log.info("This is a test................");
+    }
+
+}
